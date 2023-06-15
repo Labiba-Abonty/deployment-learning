74480b67b9a6b9ccf15a5709f4ffcf654ab77dca
Abonty18 <labibafarah2998@gmail.com>
2023-06-15 19:58:17 +0600

    1st commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartBar, faFileAlt, faMap, faPhone, faStore, faUpload, faTasks, faUser, faClipboardList, faChartLine, faDatabase, faUserMd } from '@fortawesome/free-solid-svg-icons';
+import './sidebar.css';
+import bip from '../images/biponon.png';
+
+function Sidebar() {
+     return (
+          <div className="sidebar">
+               <ul className="sidebar-menu">
+                    <img src={bip} alt="biponon" className="logo" />
+                    <button>
+                         <FontAwesomeIcon className='icon' icon={faChartBar} />
+                         Dashboard
+                    </button>
+                    <button>
+                         <FontAwesomeIcon className='icon' icon={faFileAlt} />
+                         Report
+                    </button>
+                    <button>
+                         <FontAwesomeIcon className='icon' icon={faMap} />
+                         Route Plan
+                    </button>
+                    <button>
+                         <FontAwesomeIcon className='icon' icon={faPhone} />
+                         Calls
+                    </button>
+                    <button>
+                         <FontAwesomeIcon className='icon' icon={faStore} />
+                         Outlets
+                    </button>
+                    <button>
+                         <FontAwesomeIcon className='icon' icon={faTasks} />
+                         POSM Request
+                    </button>
+                    <button>
+                         <FontAwesomeIcon className='icon' icon={faUpload} />
+                         Upload Route Plan
+                    </button>
+                    <button>
+                         <FontAwesomeIcon className='icon' icon={faClipboardList} />
+                         POSM Reporting
+                    </button>
+                    <button>
+                         <FontAwesomeIcon className='icon' icon={faUser} />
+                         Field Force
+                    </button>
+                    <button>
+                         <FontAwesomeIcon className='icon' icon={faChartLine} />
+                         Summary
+                    </button>
+                    <button>
+                         <FontAwesomeIcon className='icon' icon={faDatabase} />
+                         Data Visualization
+                    </button>
+                    <button>
+                         <FontAwesomeIcon className='icon' icon={faUserMd} />
+                         MR Module
+                    </button>
+               </ul>
+          </div>
+     );
+}
+
+export default Sidebar;
---
+++ src/pages/landingPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Sidebar from '../components/Sidebar';
+import welcomeImage from '../images/welcome.jpg';
+import wee from '../images/wee.gif';
+import './landingPage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+function LandingPage() {
+  return (
+    <div className='main'>
+      <Sidebar />
+
+
+      <div className="container">
+        <img src={wee} alt="Welcome" className="welcome-image" />
+        <p className='bold'>Welcome! Let's Get Started!</p>
+        <p className='light'>Start by requesting POSM Materials</p>
+        <button className='button'> <FontAwesomeIcon className='icon' icon={faPlus} />Request POSM</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default LandingPage;
