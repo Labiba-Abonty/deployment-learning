da412ee4e85df2b1a1b030ebad95dbe76117a61b
Abonty18 <labibafarah2998@gmail.com>
2023-06-17 17:19:35 +0600

    15th

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import React from 'react'
 import LandingPage from './pages/landingPage'
 import './App.css'
 
 function App() {
   return (
     <div>
-
       <LandingPage />
-
     </div>
-  );
+  )
 }
+
 export default App
+
